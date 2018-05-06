2229333e4db9fd4be4e8933801a433675cd8f311
Orion Miller <systmkor@gmail.com>
2018-05-05 20:58:53 -0700

    Create config.py with Skeleton Functions
    This file was created to house the functionality of interacting with a
    chomper configuration file.

---
+++ chomper/config.py
@@ -0,0 +1,10 @@
+def load(conf_file):
+    """Load a chomper configuration file, (validates configuration) if valid
+    returns conf object, otherwise returns None & throws error """
+    pass
+
+
+def is_valid(conf):
+    """Validate a chomper configuration file, returns True if no errors, false
+    otherwise"""
+    pass
