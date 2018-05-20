b69b795108e8ee3f3d2fc9d9d2596fbbf9495d31
Orion Miller <systmkor@gmail.com>
2018-05-19 17:43:01 -0700

    Add config.py Minimum Functionality
    Minimum configuration functionality exists but needs proper error
    handling, documentation, etc.

--- chomper/config.py
+++ chomper/config.py
@@ -1,10 +1,22 @@
+import yaml
+
+
 def load(conf_file):
     """Load a chomper configuration file, (validates configuration) if valid
     returns conf object, otherwise returns None & throws error """
-    pass
+
+    conf = yaml.load(conf_file.read())
+
+    if is_valid(conf):
+        return conf
+
+    return None
 
 
 def is_valid(conf):
     """Validate a chomper configuration file, returns True if no errors, false
     otherwise"""
-    pass
+
+    # Do validation checks: if any issues/errors return False
+
+    return True
