d617200369b3fcf58a13e25b39a53533c04ff0f2
Orion Miller <systmkor@gmail.com>
2018-05-27 13:50:56 +0200

    Create netconf/iptables.py File
    This file contains the implementtion specific netconf class for
    handeling network configurations on a system with iptables.

---
+++ chomper/netconf/iptables.py
@@ -0,0 +1,14 @@
+class IPTables(NetConf):
+
+    def redirect():
+        # create chomper chain
+        # add rule to chain redirect port 80 to mitmproxy port
+        # add rule to chain redirect port 443 to mitmproxy port
+        # enable chomper chain
+        pass
+
+
+    def reset():
+        # disable chomper chain
+        # delete chomper chain
+        pass
