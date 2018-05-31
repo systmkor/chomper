217175431b0620f10fa3688e28bff79f273748cd
Orion Miller <systmkor@gmail.com>
2018-05-30 20:46:34 -0700

    Fix netconf Class

--- chomper/netconf/netconf.py
+++ chomper/netconf/netconf.py
@@ -3,15 +3,15 @@
 
 import abc
 
-class NetConf():
-    __metaclass__ = abc.ABCMeta
+
+class NetConf(abc.ABC):
 
     @abc.abstractmethod
-    def redirect():
-	"""Configure system firewall/packet-filter to redirect traffic"""
+    def redirect(self):
+        """Configure system firewall/packet-filter to redirect traffic"""
         pass
 
     @abc.abstractmethod
-    def reset():
-	"""Disable system firewall/packet-filter traffic redirection"""
+    def reset(self):
+        """Disable system firewall/packet-filter traffic redirection"""
         pass
